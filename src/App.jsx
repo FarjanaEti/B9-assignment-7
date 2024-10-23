711d8b92525834cee309b8ff9f35303a7b506b1f
Farjana Eti <etimourakkhieti@gmail.com>
2024-10-23 22:07:47 +0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import SideBar from './components/SideBar'
 
 function App() {
   const [recipelist,setList]=useState([]);
+  const [prepareRecipe,setPrepareRecipies]=useState([]);
+  const[totalTime,setTime]=useState(0);
+  const [totalCalori,setCalories]=useState(0);
+
   const handleRecipeList=(recipe)=>{
     const isExist=recipelist.find(existId=>recipe.recipe_id === existId.recipe_id)
     if(!isExist){
@@ -17,6 +21,18 @@ function App() {
     else{
       alert("Id already added")
     }
+  }
+  const handlePrepareRecipe=(id)=>{
+    const deletedRecipie=recipelist.find(recipe=>recipe.recipe_id=== id)
+    
+    const updatedToTableTwo=recipelist.filter(recipe=> recipe.recipe_id !== id)
+    setList(updatedToTableTwo)
+    setPrepareRecipies([...prepareRecipe,deletedRecipie])
+  }
+
+  const Total=(time,calori)=>{
+    setTime(totalTime+time);
+    setCalories(totalCalori+calori)
   }
   return (
     <div className='container mx-auto '>
@@ -29,7 +45,13 @@ function App() {
         {/* 2 cart */}
         <Recipies handleRecipeList={handleRecipeList}></Recipies>
         {/* side bar */}
-        <SideBar recipelist={recipelist}></SideBar>
+        <SideBar recipelist={recipelist}
+         handlePrepareRecipe={handlePrepareRecipe} 
+         prepareRecipe={prepareRecipe}
+         TotalTimeCalory={Total}
+         totalTime={totalTime}
+         totalCalori={totalCalori}
+         ></SideBar>
       </section>
     </div>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,84 @@
 
-const SideBar = ({recipelist}) => {
+const SideBar = ({recipelist,handlePrepareRecipe,
+  prepareRecipe,TotalTimeCalory,totalTime, totalCalori
+    }) => {
+  console.log(recipelist)
   return (
-  <div className="md:w-1/3 border border-red-400">
-       <h1 className="text-center text-2xl font-bold">Want to Cook:{recipelist.length}</h1>                                                                                   
+  <div className="md:w-1/3 border rounded-2xl border-red-400 p-2">
+ <h1 className="text-center text-2xl font-bold">Want to Cook:{recipelist.length}</h1>    
+ <p className="border-b-2 my-4"></p>  
+ {/* Table 1 */}
+ <div className="overflow-x-auto">
+  <table className="table-auto w-full">
+    {/* head */}
+    <thead>
+      <tr className="">
+        <th></th>
+        <th>Name</th>
+        <th>time</th>
+        <th>Calories</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        recipelist.map((recipee,index)=>(
+        <tr className="border-b" key={index}>
+        <th>{index+1}</th>
+        <th>{recipee.recipe_name}</th>
+        <th>{recipee.preparing_time} Minute</th>
+        <th>{recipee.calories} Calories</th>
+        <th><button onClick={()=>
+        {handlePrepareRecipe(recipee.recipe_id) 
+          TotalTimeCalory(recipee.preparing_time,recipee.calories)}}
+         className="bg-green-600 rounded-3xl p-2 my-2">Preparing</button> </th>
+      </tr>
+        ))
+      }
+    </tbody>
+  </table>
+</div>  
+
+    {/* Table 2  */}
+    <div className="mt-5">
+      <h3 className="text-2xl font-bold text-center">Currently cooking:{prepareRecipe.length} </h3>
+
+      <p className="border-b-2 my-4"></p>  
+ {/* Table */}
+ <div className="overflow-x-auto bg-slate-100">
+  <table className="table-auto w-full">
+    {/* head */}
+    <thead>
+      <tr className="">
+        <th></th>
+        <th>Name</th>
+        <th>time</th>
+        <th>Calories</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        prepareRecipe.map((recipee,index)=>(
+        <tr className="border-b-4" key={index}>
+        <th>{recipee.recipe_id}</th>
+        <th>{recipee.recipe_name}</th>
+        <th>{recipee.preparing_time} Minute</th>
+        <th>{recipee.calories} calories</th>
+      </tr>
+        ))
+      }
+     
+    </tbody>
+    <tr className="mt-5">
+        <td></td>
+        <td></td>
+        <td>Total Time={totalTime}</td>
+        <td>Total Calori={totalCalori}</td>
+      </tr>
+  </table>
+</div>
+    </div>
    </div>
  );
 };
